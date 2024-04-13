0a8bb79f3db91575755e9b6fe1a6f0d4b46b8e23
Болоцкий Андрей Сергеевич <asbolotskiy@edu.hse.ru>
2024-04-13 15:38:02 +0300

    feat: add redirection repository

---
+++ src/main/java/org/urlshortner/urlshortener/repository/RedirectionRepository.java
@@ -0,0 +1,12 @@
+package org.urlshortner.urlshortener.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.urlshortner.urlshortener.model.Redirection;
+
+import java.net.URL;
+import java.util.Optional;
+
+public interface RedirectionRepository extends CrudRepository<Redirection,Long> {
+    Optional<Redirection> findRedirectionByUrl(URL url);
+    Optional<Redirection> findRedirectionByShortenedUrlKey(String key);
+}
