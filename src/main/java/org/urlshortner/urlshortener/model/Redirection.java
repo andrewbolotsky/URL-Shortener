d230f73cf2fb9206970d8c5257ef7eb77b096d1c
Болоцкий Андрей Сергеевич <asbolotskiy@edu.hse.ru>
2024-04-13 15:37:37 +0300

    feat: add redirection entity

---
+++ src/main/java/org/urlshortner/urlshortener/model/Redirection.java
@@ -0,0 +1,28 @@
+package org.urlshortner.urlshortener.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.net.URL;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "redirections",schema = "public",catalog = "urlshortener")
+public class Redirection {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "url")
+    private URL url;
+
+    @Column(name = "shortened_url")
+    private String shortenedUrlKey;
+
+    public Redirection(URL url, String shortenedUrlToken){
+        this.url = url;
+        this.shortenedUrlKey = shortenedUrlToken;
+    }
+}
