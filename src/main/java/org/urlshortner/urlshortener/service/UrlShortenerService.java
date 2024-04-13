b5e869f9b129da235d1e82999629d176aeb31455
Болоцкий Андрей Сергеевич <asbolotskiy@edu.hse.ru>
2024-04-13 17:29:30 +0300

    style: add javadocs

--- src/main/java/org/urlshortner/urlshortener/service/UrlShortenerService.java
+++ src/main/java/org/urlshortner/urlshortener/service/UrlShortenerService.java
@@ -15,9 +15,27 @@ public interface UrlShortenerService {
      */
     Optional<URL> validUrl(String url);
 
+    /**
+     * Get shortened url from database.
+     *
+     * @param url Full URL.
+     * @return Optional.of(...) if URL is in database, Optional.empty() otherwise.
+     */
     Optional<ShortenUrlResponse> getShortenedUrl(URL url);
 
-    ShortenUrlResponse postShortenedUrl(URL url);
+    /**
+     * Post shortened URL to database if it is not exists in database and return existing value from database if it is does exist.
+     *
+     * @param url Full URL.
+     * @return Optional.of(...) if URL insertion is not failed and key was generated correctly, Optional.empty() otherwise.
+     */
+    Optional<ShortenUrlResponse> postShortenedUrl(URL url);
 
+    /**
+     * Get full url to redirect from shortened URL.
+     *
+     * @param key Key for redirection.
+     * @return Full url to redirect.
+     */
     Optional<String> getRedirectUrl(String key);
 }
