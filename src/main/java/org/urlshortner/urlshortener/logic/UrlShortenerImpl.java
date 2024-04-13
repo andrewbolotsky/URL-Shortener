f5237c72d83ec0b72a7462a03fe538bfbd7b3bd8
Болоцкий Андрей Сергеевич <asbolotskiy@edu.hse.ru>
2024-04-13 16:29:47 +0300

    feat: add algorithm of shortening url (as implementation of url shortener interface)

---
+++ src/main/java/org/urlshortner/urlshortener/logic/UrlShortenerImpl.java
@@ -0,0 +1,43 @@
+package org.urlshortner.urlshortener.logic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+import java.util.UUID;
+
+@Component
+public class UrlShortenerImpl implements UrlShortener{
+    private final static String hashingAlgoritm = "MD5";
+    private final static int shortenedUrlKeyLength = 6;
+    MessageDigest messageDigest;
+
+    Logger logger = LoggerFactory.getLogger(UrlShortenerImpl.class);
+    UrlShortenerImpl(){
+        try {
+            this.messageDigest = MessageDigest.getInstance(hashingAlgoritm);
+        } catch(NoSuchAlgorithmException exception){
+            logger.error(String.format("No %s algorithm for generating unique keys",hashingAlgoritm));
+        }
+    }
+    @Override
+    public String generateUniqueKey(String url){
+        messageDigest.update((url + UUID.randomUUID()).getBytes());
+        StringBuilder hashedUrlBuilder = new StringBuilder();
+        for (byte currentByte : messageDigest.digest()) {
+            hashedUrlBuilder.append(String.format("%02x", currentByte));
+        }
+        String hashedString = hashedUrlBuilder.toString();
+        StringBuilder resultBuilder = new StringBuilder();
+        Random random = new Random();
+        for (int index = 0;index < shortenedUrlKeyLength;++index){
+            resultBuilder.append(hashedString.charAt(random.nextInt(hashedString.length())));
+        }
+        return resultBuilder.toString();
+
+    }
+
+}
