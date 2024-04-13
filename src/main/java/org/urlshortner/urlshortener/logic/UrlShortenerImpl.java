2ea46d63ae7cb6fa75936120ab49519d82fa99e5
Болоцкий Андрей Сергеевич <asbolotskiy@edu.hse.ru>
2024-04-13 16:53:04 +0300

    feat: add algorithm of generating unique keys make more unique keys

--- src/main/java/org/urlshortner/urlshortener/logic/UrlShortenerImpl.java
+++ src/main/java/org/urlshortner/urlshortener/logic/UrlShortenerImpl.java
@@ -10,30 +10,35 @@ import java.util.Random;
 import java.util.UUID;
 
 @Component
-public class UrlShortenerImpl implements UrlShortener{
+public class UrlShortenerImpl implements UrlShortener {
     private final static String hashingAlgoritm = "MD5";
-    private final static int shortenedUrlKeyLength = 6;
-    MessageDigest messageDigest;
+    private final static int shortenedUrlKeyLength = 7;
+
+    private final static String allowedSymbolsInUniqueKey = "0123456789qwertyuiopasdfghjklzxcvbnm";
+    private MessageDigest messageDigest;
 
     Logger logger = LoggerFactory.getLogger(UrlShortenerImpl.class);
-    UrlShortenerImpl(){
+
+    UrlShortenerImpl() {
         try {
             this.messageDigest = MessageDigest.getInstance(hashingAlgoritm);
-        } catch(NoSuchAlgorithmException exception){
-            logger.error(String.format("No %s algorithm for generating unique keys",hashingAlgoritm));
+        } catch (NoSuchAlgorithmException exception) {
+            logger.error(String.format("No %s algorithm for generating unique keys", hashingAlgoritm));
         }
     }
+
     @Override
-    public String generateUniqueKey(String url){
+    public String generateUniqueKey(String url) {
         messageDigest.update((url + UUID.randomUUID()).getBytes());
         StringBuilder hashedUrlBuilder = new StringBuilder();
         for (byte currentByte : messageDigest.digest()) {
-            hashedUrlBuilder.append(String.format("%02x", currentByte));
+            hashedUrlBuilder.append(allowedSymbolsInUniqueKey.charAt((int)
+                    currentByte & (allowedSymbolsInUniqueKey.length() - 1)));
         }
         String hashedString = hashedUrlBuilder.toString();
         StringBuilder resultBuilder = new StringBuilder();
         Random random = new Random();
-        for (int index = 0;index < shortenedUrlKeyLength;++index){
+        for (int index = 0; index < shortenedUrlKeyLength; ++index) {
             resultBuilder.append(hashedString.charAt(random.nextInt(hashedString.length())));
         }
         return resultBuilder.toString();
